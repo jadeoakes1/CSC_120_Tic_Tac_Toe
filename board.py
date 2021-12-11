35e8ae44eb8e8722a0cada3cb4b181fe8cae2398
Jade Oakes <oakesjade@gmail.com>
2021-12-10 21:25:26 -0500

    Created board and added print function

---
+++ board.py
@@ -0,0 +1,11 @@
+#
+# Jade Oakes
+# 12/10/21
+# This program is a Tic Tac Toe game and allows two users to take turns and make moves
+#
+
+board = []
+
+for i in range(3):
+    board.append(["-", "-", "-"])
+    print(board[i])
