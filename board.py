dfe673fc504eca91814782b1abdaf88170dee28b
Jade Oakes <oakesjade@gmail.com>
2021-12-12 02:26:24 -0500

    Added endgame logic (win, lose, tie) and more comments

--- board.py
+++ board.py
@@ -1,15 +1,9 @@
 #
 # Jade Oakes
 # 12/10/21
-# This program is a Tic Tac Toe game and allows two users to take turns and make moves
+# Tic Tac Toe game:
+# allows two users to take turns and place X's and O's
 #
-
-# user_input --> something 1-9
-# if they enter anything else, tell them to go again
-# check if user_input is already taken
-# add it to the board
-# check if user won (rows, columns, diagonals)
-# toggle between users upon successful moves
 
 
 # Function to display game board
@@ -17,7 +11,6 @@ def print_board(game_board):
     print("Showing board:")
     for row in game_board:
         print(row)
-    print()
 
 
 # Function to get user input
@@ -28,20 +21,24 @@ def get_user_input():
 
 
 # Function to check if mark can be placed at requested location on the board
-def check_mark(row, col, game_board):
+def check_mark(row, col, game_board):  # Check if number is in range 0-1
     if (row < 0 or row > 2) or (col < 0 or col > 2):
         print("***Invalid row or column. Numbers must be between 0-2.***")
         print("***Please mark again.***")
         print_board(game_board)
+        print()
         return False
-    elif game_board[row][col] != "-":
+    elif game_board[row][col] != "-":  # Check if row/column has already been selected
         print(f"***The location {row},{col} has already been selected.***")
         print("***Please mark somewhere else on the board.***")
         print_board(game_board)
+        print()
+        return False
     else:
         return True
 
 
+# Function to place X or O on the board
 def place_mark(player_id, game_board, row, col):
     if player_id == 1:
         game_board[row][col] = "X"
@@ -49,23 +46,107 @@ def place_mark(player_id, game_board, row, col):
         game_board[row][col] = "O"
 
 
-def check_win():
-    pass
+# Function to determine if a player got three in a row
+def check_row_win(game_board):
+    # Player 1 win possibilities
+    if game_board[0][0] == "X" and game_board[0][1] == "X" and game_board[0][2] == "X":
+        print("Player 1 wins!")
+        return True
+    elif game_board[1][0] == "X" and game_board[1][1] == "X" and game_board[1][2] == "X":
+        print("Player 1 wins!")
+        return True
+    elif game_board[2][0] == "X" and game_board[2][1] == "X" and game_board[2][2] == "X":
+        print("Player 1 wins!")
+        return True
+    # Player 2 win possibilities
+    elif game_board[0][0] == "O" and game_board[0][1] == "O" and game_board[0][2] == "O":
+        print("Player 2 wins!")
+        return True
+    elif game_board[1][0] == "O" and game_board[1][1] == "O" and game_board[1][2] == "O":
+        print("Player 2 wins!")
+        return True
+    elif game_board[2][0] == "O" and game_board[2][1] == "O" and game_board[2][2] == "O":
+        print("Player 2 wins!")
+        return True
+    else:
+        return False
+
+
+# Function to determine if a player got three in a column
+def check_col_win(game_board):
+    # Player 1 win possibilities
+    if game_board[0][0] == "X" and game_board[1][0] == "X" and game_board[2][0] == "X":
+        print("Player 1 wins!")
+        return True
+    elif game_board[0][1] == "X" and game_board[1][1] == "X" and game_board[2][1] == "X":
+        print("Player 1 wins!")
+        return True
+    elif game_board[0][2] == "X" and game_board[1][2] == "X" and game_board[2][2] == "X":
+        print("Player 1 wins!")
+        return True
+     # Player 2 win possibilities
+    elif game_board[0][0] == "O" and game_board[1][0] == "O" and game_board[2][0] == "O":
+        print("Player 2 wins!")
+        return True
+    elif game_board[0][1] == "O" and game_board[1][1] == "O" and game_board[2][1] == "O":
+        print("Player 2 wins!")
+        return True
+    elif game_board[0][2] == "O" and game_board[1][2] == "O" and game_board[2][2] == "O":
+        print("Player 2 wins!")
+        return True
+    else:
+        return False
+
+
+# Function to determine if a player got three in a diagonal
+def check_diag_win(game_board):
+    # Player 1 win possibilities
+    if game_board[0][0] == "X" and game_board[1][1] == "X" and game_board[2][2] == "X":
+        print("Player 1 wins!")
+        return True
+    elif game_board[0][2] == "X" and game_board[1][1] == "X" and game_board[2][0] == "X":
+        print("Player 1 wins!")
+        return True
+    # Player 2 win possibilities
+    elif game_board[0][0] == "O" and game_board[1][1] == "O" and game_board[2][2] == "O":
+        print("Player 2 wins!")
+        return True
+    elif game_board[0][2] == "O" and game_board[1][1] == "O" and game_board[2][0] == "O":
+        print("Player 2 wins!")
+        return True
+    else:
+        return False
+
+
+# Function to determine if there was a tie
+def check_tie(game_board):
+    if not check_row_win(game_board) and not check_col_win(game_board) and not check_diag_win(game_board) and\
+            game_board[0][0] != "-" and game_board[0][1] != "-" and game_board[0][2] != "-" and\
+            game_board[1][0] != "-" and game_board[1][1] != "-" and game_board[1][2] != "-" and\
+            game_board[2][0] != "-" and game_board[2][1] != "-" and game_board[2][2] != "-":
+        print("Tie!")
+        return True
 
 
 def main():
     # Create an empty board
     board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
-
-    # Display empty board
-    print_board(board)
-
-    # Initialize player variable
+    # Initialize variables
     player1 = 1
     player2 = 2
-    curr_player = player1
-    while curr_player:
+    curr_player = player1  # Set current player to player1
+    choice = "y"
+
+    print("Welcome to Tic Tac Toe!\n")
+    print("Player 1 is X")
+    print("Player 2 is O\n")
+
+    # Loop for continuous play until game is over
+    while choice == "y":
+        # Display empty board
+        print_board(board)
         # Get user input from player
+        print()
         print(f"Player {curr_player}, make your move")
         row_input, col_input = get_user_input()
         # Check if input is a number between 0-2
@@ -78,13 +159,22 @@ def main():
         # Place the correct mark on the board
         place_mark(curr_player, board, row_input, col_input)
         # Display board after each turn
-        print_board(board)
 
         # Toggle between player1 and player2
         if curr_player == player1:
             curr_player = player2
         else:
             curr_player = player1
+
+        # Check if there is a winner
+        if check_row_win(board) or check_col_win(board) or check_diag_win(board) or check_tie(board):
+            print_board(board)  # Reset the board
+            board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+            curr_player = player1  # Reset current player to player1
+            # Check if users want to play again
+            choice = input("\nAnother game (y/n)? ").lower()
+    else:
+        return False
 
 
 main()
